fdec8aed0dea9956b22c58ce6211f08a1c74d122
cs4alhaider <cs.alhaider@gmail.com>
2019-11-17 03:02:53 +0300

    Initial Commit

---
+++ SwiftUIMeetup/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIMeetup
+//
+//  Created by Abdullah Alhaider on 17/11/2019.
+//  Copyright © 2019 Abdullah Alhaider. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
